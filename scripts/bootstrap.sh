085415a2f3d0a8a0ff5aa43395c08cd593cc4253
Cayce House <cayce@mobilemedics.com>
2021-06-17 05:22:47 +0000

    Fixed bootstrap file

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 # Install docker
-sudo apt -y update
-sudo apt -y upgrade
-sudo apt install docker
+sudo yum update -y
+sudo yum install docker
+
+# Enable and start docker
+sudo systemctl --now enable docker
 
 # Install docker-compose
 sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
